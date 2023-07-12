aad15011420db28447f89fdffec80839427089c2
panyq357 <panyq357@qq.com>
2023-07-12 10:56:23 +0800

    add a pipeline for batch enrichment analysis

--- oryzabase_anno_pipeline.R
+++ oryzabase_anno_pipeline.R
@@ -25,11 +25,17 @@ main = function() {
         for (fc in config[["fc_list"]]) {
 
             gene_list = select_gene_from_de_result(de_result_df, config[["p_column_name"]], p, config[["fc_column_name"]], fc)
+            gsea_gene_list = get_gsea_gene_from_de_result(de_result_df, config[["fc_column_name"]])
 
             enrich_result_list = oryzabase_enricher(gene_list, onto_list)
+            gsea_result_list = oryzabase_GSEA(gsea_gene_list, onto_list)
+            names(gsea_result_list) = paste0(names(gsea_result_list), "_GSEA")
 
-            save_enrich_result_list(enrich_result_list, file.path(config[["outdir"]], sprintf("p%s.fc%s.xlsx", p, config$fc_rev_fun(fc))))
-            save_dotplot(enrich_result_list, file.path(config[["outdir"]], sprintf("p%s.fc%s.pdf", p, config$fc_rev_fun(fc))))
+            all_result_list = c(enrich_result_list, gsea_result_list)
+
+            save_result_list(all_result_list, file.path(config[["outdir"]], sprintf("p%s.fc%s.xlsx", p, config$fc_rev_fun(fc))))
+            save_dotplot(all_result_list, file.path(config[["outdir"]], sprintf("p%s.fc%s.pdf", p, config$fc_rev_fun(fc))))
+            saveRDS(all_result_list, file.path(config[["outdir"]], sprintf("p%s.fc%s.rds", p, config$fc_rev_fun(fc))))
         }
     }
 }
@@ -44,6 +50,14 @@ select_gene_from_de_result = function(de_result_df, p_column_name, max_p, fc_col
         row.names()
     gene_list[["universe"]] = row.names(de_result_df)
     return(gene_list)
+}
+
+get_gsea_gene_from_de_result = function(de_result_df, fc_column_name) {
+    gsea_gene_list = de_result_df[[fc_column_name]] |>
+        setNames(row.names(de_result_df)) |>
+        sort(decreasing=T)
+    class(gsea_gene_list) = c("numeric", "gsea_gene_list")
+    return(gsea_gene_list)
 }
 
 read_oryzabase_onto = function(xlsx_path, sheet_list) {
@@ -75,16 +89,35 @@ oryzabase_enricher = function(gene_list, onto_list) {
     return(enrich_result_list)
 }
 
-save_enrich_result_list = function(enrich_result_list, out_xlsx) {
-    enrich_result_list |>
+oryzabase_GSEA = function(gsea_gene_list, onto_list) {
+    gsea_result_list = list()
+    class(gsea_result_list) = "gsea_result_list"
+    for (onto_name in names(onto_list)) {
+        res = GSEA(
+            geneList=gsea_gene_list,
+            TERM2GENE=onto_list[[onto_name]][c("OntoID", "GeneID")],
+            TERM2NAME=onto_list[[onto_name]][c("OntoID", "Description")],
+            pvalueCutoff = 1
+        )
+        if (is.null(res)) {
+            warning(sprintf("%s result is NULL.", onto_name))
+        } else {
+            gsea_result_list[[onto_name]] = res
+        }
+    }
+    return(gsea_result_list)
+}
+
+save_result_list = function(result_list, out_xlsx) {
+    result_list |>
         lapply(as.data.frame) |>
         writexl::write_xlsx(out_xlsx)
 }
 
-save_dotplot = function(enrich_result_list, out_pdf) {
+save_dotplot = function(result_list, out_pdf) {
     pdf(out_pdf)
-    for (res in enrich_result_list)
-        dotplot(res) |> plot()
+    for (res_name in names(result_list))
+        dotplot(result_list[[res_name]], title=res_name) |> plot()
     dev.off()
 }
 
